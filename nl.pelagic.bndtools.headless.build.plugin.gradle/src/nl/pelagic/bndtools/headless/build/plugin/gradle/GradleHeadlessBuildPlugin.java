f3456105a8b3e9855a390374d67f3f9880c75be3
Ferry Huberts <ferry.huberts@pelagic.nl>
2015-10-29 19:50:12 +0100

    Revert "Replace build files by default"
    This reverts commit 284ec7d0ff9b225fd1004f87ca9c5f5e5dc8cfb9.
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- nl.pelagic.bndtools.headless.build.plugin.gradle/src/nl/pelagic/bndtools/headless/build/plugin/gradle/GradleHeadlessBuildPlugin.java
+++ nl.pelagic.bndtools.headless.build.plugin.gradle/src/nl/pelagic/bndtools/headless/build/plugin/gradle/GradleHeadlessBuildPlugin.java
@@ -74,10 +74,10 @@ public class GradleHeadlessBuildPlugin implements HeadlessBuildPlugin {
         File workspaceRoot = projectDir.getParentFile();
 
         String baseDir = "templates/root/";
-        Collection<File> files1 = copier.addOrRemoveDirectory(workspaceRoot, baseDir, "/", add ? CopyMode.REPLACE : CopyMode.REMOVE);
+        Collection<File> files1 = copier.addOrRemoveDirectory(workspaceRoot, baseDir, "/", add ? CopyMode.ADD : CopyMode.REMOVE);
 
         baseDir = "templates/cnf/";
-        Collection<File> files2 = copier.addOrRemoveDirectory(projectDir, baseDir, "/", add ? CopyMode.REPLACE : CopyMode.REMOVE);
+        Collection<File> files2 = copier.addOrRemoveDirectory(projectDir, baseDir, "/", add ? CopyMode.ADD : CopyMode.REMOVE);
 
         files1.addAll(files2);
 
@@ -85,7 +85,7 @@ public class GradleHeadlessBuildPlugin implements HeadlessBuildPlugin {
             for (File file : files1) {
                 String warning;
                 if (add) {
-                    warning = String.format("Existing Gradle build file %s was overwritten", file);
+                    warning = String.format("Not overwriting existing Gradle build file %s", file);
                 } else {
                     warning = String.format("Unable to remove Gradle build file %s", file);
                 }
