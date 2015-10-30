7381e0fcf688fc01f34cd8b7d963803dfd1a726c
Ferry Huberts <ferry.huberts@pelagic.nl>
2015-10-30 16:02:56 +0100

    Fixup the GradleHeadlessBuildPluginInformation class
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- nl.pelagic.bndtools.headless.build.plugin.gradle/src/nl/pelagic/bndtools/headless/build/plugin/gradle/GradleHeadlessBuildPluginInformation.java
+++ nl.pelagic.bndtools.headless.build.plugin.gradle/src/nl/pelagic/bndtools/headless/build/plugin/gradle/GradleHeadlessBuildPluginInformation.java
@@ -7,18 +7,21 @@ package nl.pelagic.bndtools.headless.build.plugin.gradle;
 import org.bndtools.api.NamedPlugin;
 
 public class GradleHeadlessBuildPluginInformation implements NamedPlugin {
-    private static final String NAME = "Gradle (by Pelagic)";
-    private static final boolean ENABLED_BY_DEFAULT = false;
-    private static final boolean DEPRECATED = false;
+    public static final String NAME = "Gradle (by Pelagic)";
+    public static final boolean ENABLED_BY_DEFAULT = false;
+    public static final boolean DEPRECATED = false;
 
+    @Override
     public String getName() {
         return NAME;
     }
 
+    @Override
     public boolean isEnabledByDefault() {
         return ENABLED_BY_DEFAULT;
     }
 
+    @Override
     public boolean isDeprecated() {
         return DEPRECATED;
     }
